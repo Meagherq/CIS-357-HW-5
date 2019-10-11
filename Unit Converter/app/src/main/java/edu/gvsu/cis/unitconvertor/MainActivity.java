b376d3e4822f4ee5420e30e71b8888f1f422e44c
gustafsn <gustafsn@mail.gvsu.edu>
2019-10-10 21:21:39 -0400

    layout setup. code stuff

--- Unit Converter/app/src/main/java/edu/gvsu/cis/unitconvertor/MainActivity.java
+++ Unit Converter/app/src/main/java/edu/gvsu/cis/unitconvertor/MainActivity.java
@@ -3,6 +3,7 @@ package edu.gvsu.cis.unitconvertor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button calc = (Button) findViewById(R.id.Calculate);
+        calc.setOnLongClickListener(v -> {System.out.println("Long Click");});
+
+
     }
 }
